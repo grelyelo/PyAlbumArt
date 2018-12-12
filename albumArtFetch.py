85c5c5ea2c86bd0892795a2d56eae056acd4e13e
Timothy L <tlesgaux@protonmail.ch>
2018-12-11 21:57:14 -0600

    added first file for downloading album art

---
+++ albumArtFetch.py
@@ -0,0 +1,18 @@
+import http.client
+from bs4 import BeautifulSoup as bs
+
+def fetchAlbumArt(aName, aArtist):
+    #We are using coverartarchive.org API
+    
+
+    #first, we need to find the album from musicbrainz.org
+    #so we search the album and and get the mbid for the first result
+
+    mbid = ""
+
+
+    caaConnection = http.client.HTTPSConnection(
+    results = 
+    topResult = 
+    
+    
