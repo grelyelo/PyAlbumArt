7e3900cfb2c66fb158f45ed6c0db5d3b9944dc2a
Timothy L <tlesgaux@protonmail.ch>
2018-12-25 19:55:55 -0600

    Created Search functionality

--- albumArtFetch.py
+++ albumArtFetch.py
@@ -1,18 +1,22 @@
-import http.client
+import urllib.request
+import urllib.parse
 from bs4 import BeautifulSoup as bs
 
-def fetchAlbumArt(aName, aArtist):
-    #We are using coverartarchive.org API
-    
+BASE_MB_SEARCH_URL = "https://musicbrainz.org/ws/2/release-group"
 
+def fetchAlbumArt(artist, album):
+    #We are using coverartarchive.org API  
     #first, we need to find the album from musicbrainz.org
     #so we search the album and and get the mbid for the first result
 
-    mbid = ""
+  
 
-
-    caaConnection = http.client.HTTPSConnection(
-    results = 
-    topResult = 
+    mbid = "" #Music Brainz ID number, which we will find using the search function
+    searchQuery = urllib.parse.quote_plus("%s %s" % (artist, album))#make search query http parsable 
+    search_url = BASE_MB_SEARCH_URL+searchQuery
+    response = urllib.request.openurl(search_url) #Make HTTP request to database
+    results_xml = response.read()                 #Store the XML formatted response in memory
+    
+    #topResult = 
     
     
