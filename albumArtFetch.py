81d57044a226ddbe752151c8ec8660f1c6bb39b3
Timothy L <tlesgaux@protonmail.ch>
2018-12-30 10:40:02 -0600

    changed function name + more code

--- albumArtFetch.py
+++ albumArtFetch.py
@@ -1,24 +1,30 @@
 import urllib.request
 import urllib.parse
+import json
 from bs4 import BeautifulSoup as bs
 
 BASE_MB_SEARCH_URL = "https://musicbrainz.org/ws/2/release-group/?query="
+BASE_CAA_SEARCH_URL = "https://coverartarchive.org/release-group/"
 
-def fetchAlbumArt(artist, album):
+def getLinkToAlbumArt(artist, album, mbid = ""):
     #We are using coverartarchive.org API  
     #first, we need to find the album from musicbrainz.org
     #so we search the album and and get the mbid for the first result
 
-  
+    
+    if(len(mbid) > 0):
+        #If provided a specific mbid, we can skip the search function and simply download the image. 
+        coverListingJSON = urllib.request.urlopen(BASE_CAA_SEARCH_URL+mbid).read()
+        
 
-    mbid = "" #Music Brainz ID number, which we will find using the search function
+   #Music Brainz ID number, which we will find using the search function
     searchQuery = urllib.parse.quote_plus("%s %s" % (artist, album))#make search query http parsable 
-    search_url = BASE_MB_SEARCH_URL+searchQuery
-    print(search_url)
-#    response = urllib.request.urlopen(search_url) #Make HTTP request to database
-#    results_xml = response.read()                 #Store the XML formatted response in memory
-    
-#    print(results_xml) #This is for testing
-    #topResult = 
-    
-    
+    searchURL = BASE_MB_SEARCH_URL+searchQuery
+
+    response = urllib.request.urlopen(searchURL) #Make HTTP request to database
+    resultsXML = response.read()                 #Store the XML formatted response in memory
+    bsXML = bs(resultsXML,"xml") #Create BeautifulSoup Parsable XML
+
+    results = bsXML.find_all('release-group') 
+    firstResult = results[0]#It is most likely that the first result will be correct, so we will assume it is correct. 
+        
