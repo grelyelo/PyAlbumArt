13cd6aca0f5d3c5e3a5734a3aaa75034f9ea868e
Timothy L <tlesgaux@protonmail.ch>
2019-01-07 15:13:04 -0600

    Added function searchMusicbrainz for better code layout

--- albumArtFetch.py
+++ albumArtFetch.py
@@ -12,7 +12,18 @@ def getLinkToAlbumArt(artist, album, mbid = "", art="front"):
     #first, we need to find the album from musicbrainz.org
     #so we search the album and and get the mbid for the first result
 
-	if(len(mbid) == 0):#If we are not provided an mbid, search for correct mbid. Otherwise, skip the following. 
+	if(len(mbid) == 0):#If we are not provided an mbid, search for correct mbid.
+		mbid = searchMusicbrainz(artist, album) 
+			
+	if(len(mbid) > 0):
+		#If we have an MBID, we can try to search the cover art archive for the art. 
+		caaHeadResponse = requests.head(BASE_CAA_SEARCH_URL+mbid+"/"+art, allow_redirects=True)
+		return caaHeadResponse.url
+	else:
+		return None
+
+
+def searchMusicbrainz(artist, album):#Try to search Musicbrainz.org for album. 
 		artist=urllib.parse.quote_plus(artist)
 		album=urllib.parse.quote_plus(album)
 		searchQuery = "artist:"+artist+"%20AND%20release:"+album+"&limit=1"
@@ -27,13 +38,7 @@ def getLinkToAlbumArt(artist, album, mbid = "", art="front"):
 		
 		try:
 			mbid = results[0].get("id")
+			return mbid
 		except KeyError:
 			print("Could not get mbid from first album result. Possible bad musicbrainz.org response")
 			return None
-			
-	if(len(mbid) > 0):
-		#If we have an MBID, we can try to search the cover art archive for the art. 
-		caaHeadResponse = requests.head(BASE_CAA_SEARCH_URL+mbid+"/"+art, allow_redirects=True)
-		return caaHeadResponse.url
-	else:
-		return None
